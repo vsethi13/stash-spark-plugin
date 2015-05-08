90f9841a1795aa2fe3c61db434ac963cbe94d5f4
Vivek Sethi <vivekse@cisco.com>
2015-05-08 22:02:18 +0530

    adding post-receive hook module to the plugin

---
+++ src/test/java/it/com/cisco/stash/plugin/hook/SparkNotifyHookWiredTest.java
@@ -0,0 +1,19 @@
+package it.com.cisco.stash.plugin.hook;
+
+import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Sagar on 08/05/15.
+ */
+@RunWith(AtlassianPluginsTestRunner.class)
+public class SparkNotifyHookWiredTest {
+
+    @Test
+    public void test(){
+       assertTrue(true);
+    }
+}
---
+++ src/test/java/ut/com/cisco/stash/plugin/hook/SparkNotifyHookUnitTest.java
@@ -0,0 +1,15 @@
+package ut.com.cisco.stash.plugin.hook;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Created by Sagar on 08/05/15.
+ */
+public class SparkNotifyHookUnitTest {
+
+    @Test
+    public void test(){
+       assertTrue(true);
+    }
+}
