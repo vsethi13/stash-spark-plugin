b391457fef17e6f95104247d576a7498c486a6eb
Vivek Sethi <vivekse@cisco.com>
2015-05-13 20:16:49 +0530

    adding repo and commit URLs; code clean up

---
+++ src/main/java/com/cisco/stash/plugin/hook/Notifier.java
@@ -0,0 +1,96 @@
+package com.cisco.stash.plugin.hook;
+
+import com.atlassian.stash.commit.Commit;
+import com.atlassian.stash.commit.CommitService;
+import com.atlassian.stash.commit.CommitsBetweenRequest;
+import com.atlassian.stash.hook.repository.RepositoryHookContext;
+import com.atlassian.stash.nav.NavBuilder;
+import com.atlassian.stash.repository.RefChange;
+import com.atlassian.stash.repository.RefChangeType;
+import com.atlassian.stash.repository.Repository;
+import com.atlassian.stash.user.StashAuthenticationContext;
+import com.atlassian.stash.user.StashUser;
+import com.atlassian.stash.util.PageRequestImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.atlassian.stash.scm.git.GitRefPattern;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Sagar on 12/05/15.
+ */
+public class Notifier {
+
+    private static final Logger log = LoggerFactory.getLogger(Notifier.class);
+    private final StashAuthenticationContext stashAuthenticationContext;
+    private final CommitService commitService;
+    private final NavBuilder navBuilder;
+
+    public Notifier(StashAuthenticationContext stashAuthenticationContext, CommitService commitService, NavBuilder navBuilder){
+        this.stashAuthenticationContext = stashAuthenticationContext;
+        this.commitService = commitService;
+        this.navBuilder = navBuilder;
+    }
+
+    public StringBuilder createNotification(RepositoryHookContext repositoryHookContext, Collection<RefChange> refChanges){
+        StringBuilder notification = new StringBuilder(1000);
+        Map<String, String> commitLinks = new HashMap<String, String>();
+        StashUser stashUser = stashAuthenticationContext.getCurrentUser();
+        Repository repository = repositoryHookContext.getRepository();
+        notification.append(stashUser.getDisplayName() + "[" + stashUser.getEmailAddress() + "] ");
+        notification.append("committed to " + refChanges.size() + " branch(es) ");
+        notification.append("at " + repository.getProject().getName() + "/" + repository.getName());
+        notification.append("\n");
+
+        for(RefChange refChange : refChanges){
+            if(refChange.getType() == RefChangeType.ADD){
+                notification.append("New ref " + StringUtils.removeStart(refChange.getRefId(), GitRefPattern.HEADS.getPath()) + " has been added to the repo");
+                notification.append("\n");
+            }
+            else if(refChange.getType() == RefChangeType.DELETE){
+                notification.append("The ref " + StringUtils.removeStart(refChange.getRefId(), GitRefPattern.HEADS.getPath()) + " has been deleted from the repo");
+                notification.append("\n");
+            }
+            else if(refChange.getType() == RefChangeType.UPDATE){
+                notification.append("On ref " + StringUtils.removeStart(refChange.getRefId(), GitRefPattern.HEADS.getPath()) + ":");
+                notification.append("\n");
+
+                CommitsBetweenRequest commitsBetweenRequest;
+                commitsBetweenRequest = new CommitsBetweenRequest.Builder(repository)
+                        .include(refChange.getToHash())
+                        .exclude(refChange.getFromHash())
+                        .build();
+
+                //TODO: do something about the hardcoded literals
+                //TODO: limit amount of commit info to be displayed
+                for (Commit commit : commitService.getCommitsBetween(commitsBetweenRequest, new PageRequestImpl(0, 100)).getValues()) {
+                    notification.append("- " + commit.getMessage() + "(" + commit.getDisplayId() + ") ");
+                    notification.append("@ " + commit.getAuthorTimestamp());
+                    notification.append("\n");
+                    commitLinks.put(commit.getDisplayId(), navBuilder.repo(repository).commit(commit.getId()).buildConfigured());
+                }
+            }
+        }
+        notification.append("Repo URL: " + navBuilder.repo(repository).buildConfigured());
+        notification.append("\n");
+        if(commitLinks.size() > 0) {
+            notification.append("Commit URL(s): \n");
+            for (Map.Entry<String, String> commitEntry : commitLinks.entrySet()) {
+                notification.append("- " + commitEntry.getKey() + ": " + commitEntry.getValue());
+                notification.append("\n");
+            }
+        }
+        return notification;
+    }
+
+    public void publishNotification(StringBuilder notification){
+        int notificationLength = notification.length();
+        if(notification.charAt(notificationLength-1) == '\n')
+            notification.deleteCharAt(notificationLength-1);
+        System.out.println(notification);
+    }
+}
--- src/main/java/com/cisco/stash/plugin/hook/SparkNotifyHook.java
+++ src/main/java/com/cisco/stash/plugin/hook/SparkNotifyHook.java
@@ -1,55 +1,40 @@
 package com.cisco.stash.plugin.hook;
 
-import com.atlassian.stash.commit.Commit;
+import com.atlassian.event.api.EventListener;
 import com.atlassian.stash.commit.CommitService;
-import com.atlassian.stash.commit.CommitsBetweenRequest;
+import com.atlassian.stash.event.pull.PullRequestOpenedEvent;
 import com.atlassian.stash.hook.repository.*;
+import com.atlassian.stash.nav.NavBuilder;
 import com.atlassian.stash.repository.*;
 import com.atlassian.stash.setting.*;
 import com.atlassian.stash.user.StashAuthenticationContext;
-import com.atlassian.stash.user.StashUser;
-import com.atlassian.stash.util.PageRequestImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collection;
 
 public class SparkNotifyHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator {
 
     private final CommitService commitService;
     private final StashAuthenticationContext stashAuthenticationContext;
+    private final NavBuilder navBuilder;
 
-    public SparkNotifyHook(CommitService commitService, StashAuthenticationContext stashAuthenticationContext) {
+    private static final Logger log = LoggerFactory.getLogger(SparkNotifyHook.class);
+
+    public SparkNotifyHook(StashAuthenticationContext stashAuthenticationContext, CommitService commitService, NavBuilder navBuilder) {
         this.commitService = commitService;
         this.stashAuthenticationContext = stashAuthenticationContext;
+        this.navBuilder = navBuilder;
     }
 
     /**
      * Connects to a configured URL to notify of all changes.
      */
     @Override
-    public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) {
-        StashUser currentUser = stashAuthenticationContext.getCurrentUser();
-        System.out.println(currentUser.getDisplayName() + "[" + currentUser.getEmailAddress() + "]" + " committed to " + refChanges.size() + " branch(es) at " + context.getRepository().getProject().getName() + "/" + context.getRepository().getName());
+    public void postReceive(RepositoryHookContext repositoryHookContext, Collection<RefChange> refChanges) {
 
-        for (RefChange refChange : refChanges) {
-            if (refChange.getType() == RefChangeType.ADD) {
-                System.out.println("New ref '" + refChange.getRefId() + "' created.");
-            } else if (refChange.getType() == RefChangeType.UPDATE) {
-                System.out.println("On ref '" + refChange.getRefId() + "':");
-
-                CommitsBetweenRequest commitsBetweenRequest;
-                commitsBetweenRequest = new CommitsBetweenRequest.Builder(context.getRepository())
-                        .include(refChange.getToHash())
-                        .exclude(refChange.getFromHash())
-                        .build();
-
-                //todo: do something about the hardcoded literals
-                for (Commit commit : commitService.getCommitsBetween(commitsBetweenRequest, new PageRequestImpl(0, 100)).getValues()) {
-                    System.out.println("- " + commit.getMessage() + "(" + commit.getDisplayId() + ") @ " + commit.getAuthorTimestamp());
-                }
-
-            } else if (refChange.getType() == RefChangeType.DELETE) {
-                System.out.println("The ref '" + refChange.getRefId() + "' deleted.");
-            }
-        }
+        Notifier notifier = new Notifier(stashAuthenticationContext, commitService, navBuilder);
+        notifier.publishNotification(notifier.createNotification(repositoryHookContext, refChanges));
     }
 
     @Override
