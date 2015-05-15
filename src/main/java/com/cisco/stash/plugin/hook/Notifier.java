8bfc29d5bc8737beff27a67b7b9534e696a4df39
Vivek Sethi <vivekse@cisco.com>
2015-05-15 17:38:20 +0530

    adding basic spark integration

--- src/main/java/com/cisco/stash/plugin/hook/Notifier.java
+++ src/main/java/com/cisco/stash/plugin/hook/Notifier.java
@@ -20,6 +20,11 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.cisco.dft.cd.spark.intg.service.pojo.HttpResponseEntity;
+import com.cisco.dft.cd.spark.intg.service.pojo.Message;
+import com.cisco.dft.cd.spark.intg.service.service.SparkIntegrationService;
+import com.cisco.dft.cd.spark.intg.service.util.Constants.PlatformType;
+
 /**
  * Created by Sagar on 12/05/15.
  */
@@ -91,6 +96,13 @@ public class Notifier {
         int notificationLength = notification.length();
         if(notification.charAt(notificationLength-1) == '\n')
             notification.deleteCharAt(notificationLength-1);
-        System.out.println(notification);
+        Message message = new Message();
+        message.setMessage(notification.toString());
+        message.setPlatformType(PlatformType.STASH);
+        message.setRoomId("c60f38e0-ef86-11e4-9df9-270f179cbc2c");
+        SparkIntegrationService.publishMessage(message);
+//        HttpResponseEntity response = SparkIntegrationService.publishMessage(message);
+//        System.out.println("response: " + response.getStatusCode());
+//        System.out.println(notification);
     }
 }
