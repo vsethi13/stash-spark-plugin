ed0eee859261e166870f7ce11718cdd202759b06
Vivek Sethi <vivekse@cisco.com>
2015-05-19 10:42:35 +0530

    updated to spark sdk v0.0.4

--- src/main/java/com/cisco/stash/plugin/util/Notifier.java
+++ src/main/java/com/cisco/stash/plugin/util/Notifier.java
@@ -1,12 +1,10 @@
 package com.cisco.stash.plugin.util;
 
+import com.cisco.dft.cd.spark.intg.pojo.Actor;
+import com.cisco.dft.cd.spark.intg.pojo.Message;
+import com.cisco.dft.cd.spark.intg.service.impl.SparkIntegrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.cisco.dft.cd.spark.intg.service.pojo.HttpResponseEntity;
-import com.cisco.dft.cd.spark.intg.service.pojo.Message;
-import com.cisco.dft.cd.spark.intg.service.service.SparkIntegrationService;
-import com.cisco.dft.cd.spark.intg.service.util.Constants.PlatformType;
 
 /**
  * Created by Sagar on 12/05/15.
@@ -20,17 +18,24 @@ public class Notifier {
     public static final String ROOM_ID = "roomId";
     public static final String BEARER_TOKEN = "bearerToken";
 
+    //Spark actor details:
+    public static final String USERNAME = "platform-stash";
+    public static final String PASSWORD = "W8.5)M1)/17=y6cTirbVL)oVc|0jF$M0";
+    public static final String ACTOR_ID = "5a86a12b-c580-4ada-b4c0-9d0dfe2d72cd";
+
     private static final Logger log = LoggerFactory.getLogger(Notifier.class);
 
     public void publishNotification(String roomId, StringBuilder notification){
+
+        Actor actor = new Actor();
+        actor.setUsername(USERNAME);
+        actor.setPassword(PASSWORD);
+        actor.setUid(ACTOR_ID);
+
         Message message = new Message();
         message.setMessage(notification.toString());
-        message.setPlatformType(PlatformType.STASH);
         message.setRoomId(roomId);
-        SparkIntegrationService.publishMessage(message);
-//        HttpResponseEntity response = SparkIntegrationService.publishMessage(message);
-//        System.out.println("response: " + response.getStatusCode());
-//        System.out.println(roomId);
-//        System.out.println(notification);
+        message.setActor(actor);
+        new SparkIntegrationService().publishMessage(message);
     }
 }
